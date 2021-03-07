3d85ba019e5eb051103d46d369169f498eb37a2e
Marouane Souda <marsou431@gmail.com>
2021-03-07 23:17:30 +0100

    add final touches

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -4,10 +4,51 @@ import { useState } from 'react';
 import Tabs from './Tabs/Tabs.jsx';
 import Main from './Main/Main.jsx';
 
-const header = 'Bookmark in one click';
-const paragraph = `Organise your bookmarks however you like.
-                   Our simple drag-and-drop interface gives you complete
-                   control over how you manage your favourite sites.`;
+// const header = 'Bookmark in one click';
+// const paragraph = `Organise your bookmarks however you like.
+//                    Our simple drag-and-drop interface gives you complete
+//                    control over how you manage your favourite sites.`;
+
+const headers = {
+    header1: 'Bookmark in one click',
+    header2: 'Intelligent search',
+    header3: 'Share your bookmarks Easily',
+}
+
+const paragraphs = {
+    paragraph1: `organise your bookmarks however you like.
+                 Our simple drag-and-drop interface gives you complete
+                 control over how you manage your favourite sites.`,
+    paragraph2: `Our powerful search feature will help you find saved sites in no time at all. 
+                 No need to trawl through all of your bookmarks.`,
+    paragraph3: `share your bookmarks and collections with others. 
+                 Create a shareable link that you can send at the click of a button.`,
+}
+
+function fetchHeaderAndParagraph(selectedTab) {
+    switch(selectedTab) {
+        case 'Simple Bookmarking':
+            return ({ 
+                header: headers.header1,
+                paragraph: paragraphs.paragraph1
+            });
+        case 'Speedy Searching':
+            return ({ 
+                header: headers.header2,
+                paragraph: paragraphs.paragraph2
+            });
+        case 'Easy Sharing':
+            return ({ 
+                header: headers.header3,
+                paragraph: paragraphs.paragraph3
+            });
+        default:
+            return ({ 
+                header: headers.header1,
+                paragraph: paragraphs.paragraph1
+            });
+    }
+}
 
 const TabsContent = styled.div`
     display: flex;
@@ -22,14 +63,16 @@ const TabsContent = styled.div`
 function Features() {
     const [selectedTab, setSelectedTab] = useState('Simple Bookmarking');
 
-    const handleClick = (e) => setSelectedTab(e.target.textContent);    
+    const handleClick = (e) => {
+        e.target.nodeName === 'SPAN' && setSelectedTab(e.target.textContent);
+    };    
 
     return (
         <>
             <Tabs selectedTab={selectedTab} handleClick={handleClick} />
             <TabsContent>
                 <Main selectedTab={selectedTab} />
-                <Text isInsideTab={true} header={header} paragraph={paragraph} />
+                <Text isInsideTab={true} header={fetchHeaderAndParagraph(selectedTab).header} paragraph={fetchHeaderAndParagraph(selectedTab).paragraph} />
             </TabsContent>
         </>
     )
--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -12,38 +12,34 @@ const questions = [
     {
         id: 1,
         question: 'What is bookmark',
-        answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Fames ac turpis egestas sed. Sodales neque sodales ut etiam sit. 
-                 Vitae tempus quam pellentesque nec nam. 
-                 Eget gravida cum sociis natoque penatibus et magnis.`
+        answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                 Fusce tincidunt justo eget ultricies fringilla.
+                 Phasellus blandit ipsum quis quam ornare mattis.`
     },
     {
         id: 2,
         question: 'How can I request a new browser',
-        answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Fames ac turpis egestas sed. Sodales neque sodales ut etiam sit. 
-                 Vitae tempus quam pellentesque nec nam. 
-                 Eget gravida cum sociis natoque penatibus et magnis.`
+        answer: `Vivamus luctus eros aliquet convallis ultricies.
+                 Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.
+                 Vivamus luctus eros aliquet convallis ultricies.
+                 Mauris augue massa, ultricies non ligula.
+                 Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. 
+                 Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.`
     },
     {
         id: 3,
         question: 'Is there a mobile app',
-        answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Fames ac turpis egestas sed. Sodales neque sodales ut etiam sit. 
-                 Vitae tempus quam pellentesque nec nam. 
-                 Eget gravida cum sociis natoque penatibus et magnis.`
+        answer: `Sed consectetur quam id neque fermentum accumsan.
+                 Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget.
+                 Cras in ligula quis est pharetra mattis sit amet pharetra purus.
+                 Sed sollicitudin ex et ultricies bibendum.`
     },
     {
         id: 4,
         question: 'What about other Chromium browsers',
-        answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Fames ac turpis egestas sed. Sodales neque sodales ut etiam sit. 
-                 Vitae tempus quam pellentesque nec nam. 
-                 Eget gravida cum sociis natoque penatibus et magnis.`
+        answer: `Integer condimentum ipsum id imperdiet finibus. 
+                 Vivamus in placerat mi, at euismod dui. 
+                 Aliquam vitae neque eget nisl gravida pellentesque non ut velit.`
     },
 ];
 
